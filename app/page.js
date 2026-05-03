109df22ada6129cc1cba48feb784f4612746134b
Akshay Upadhyay <akz@amazon.com>
2026-05-02 21:20:42 -0700

    auto promote v1

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Production v2: Dark Deploy Test</h1>
-      <p>This deployment should NOT auto-promote to production alias</p>
-    </div>
-  );
+  return <div><h1>Auto Promote v1</h1></div>;
 }
